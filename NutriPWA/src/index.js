5413b528f1f097373a2812fe6bad66bcbc03f1c8
Dilara <dilara.tank1@student.uva.nl>
2023-02-15 18:58:22 +0100

    food recognition pipeline

---
+++ NutriPWA/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import swDev from './swDev';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+swDev();
