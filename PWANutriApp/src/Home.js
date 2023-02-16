b7d00c92d6cce613991dbb1269fabd9467fe8244
Dilara <dilara.tank1@student.uva.nl>
2023-02-16 19:13:18 +0100

    sad model integration attempt

--- PWANutriApp/src/Home.js
+++ PWANutriApp/src/Home.js
@@ -3,7 +3,10 @@ import FoodInfo from './FoodInfo';
 import {TbRepeat} from 'react-icons/tb';
 import {FiCamera} from 'react-icons/fi';
 import {MdOutlineSendToMobile} from 'react-icons/md';
+import ndarray from 'ndarray';
+import ops from 'ndarray-ops';
 
+import { food101topK } from './utils';
 
 export default  function Home()
 {
@@ -11,6 +14,7 @@ export default  function Home()
     const photoRef = useRef(null);
     const [hasPhoto, setHasPhoto] = useState(false);
     const [hasFood, setHasFood] = useState(false);
+    const [model, setModel] = useState(false);
 
 
     const getVideo = () => {
@@ -28,8 +32,26 @@ export default  function Home()
         })
     }
 
+    function loadModel() {
+        console.log('Loading Model');
+        const model = new window.KerasJS.Model({
+          filepaths: {
+            model: 'model.json' ,
+            weights: 'model4b.10-0.68_weights.buf',
+            metadata: 'model4b.10-0.68_metadata.json'
+          },
+        //   gpu: this.state.hasWebgl,
+          layerCallPauses: true
+        });
+
+        const waitTillReady = model.ready();
+        setModel(waitTillReady);
+
+    }
+
     useEffect(() => {
-        getVideo()
+        getVideo();
+        loadModel();
     }, [videoRef]);
 
     const takePhoto = () => {
