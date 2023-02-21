4872505b198812b122f2b37fc19394f3f6d28d61
aespogom <100383119@alumnos.uc3m.es>
2023-02-21 20:09:09 +0100

    Bug server workers

--- PWANutriApp/public/sw.js
+++ PWANutriApp/public/sw.js
@@ -11,7 +11,9 @@ this.addEventListener("install", (event) => {
                 '/bootstrap.min.css',
                 '/index.html',
                 '/',
-                "/home"
+                "/home",
+                "/favicon.ico",
+                "/manifest.json"
             ]).then(() => this.skipWaiting());
         })
     );
@@ -25,7 +27,9 @@ this.addEventListener('activate', event => {
 //fetch from cache for offline modus
 this.addEventListener("fetch", (event)=>{
 
-    if(!navigator.online)
+    if (event.request )
+
+    if(!navigator.onLine)
     {
         event.respondWith(
             caches.match(event.request).then((resp)=>{
@@ -55,35 +59,35 @@ this.addEventListener("fetch", (event)=>{
     }
 })
 
-// Respond to a server push with a user notification.
-this.addEventListener('push', (event) => {
-    if (Notification.permission === "granted") {
-        const notificationText = event.data.text();
-        const showNotification = navigator.serviceWorker.ready.then((r) => {
-            r.showNotification('Sample PWA', {
-                body: notificationText
-            });
-        })
-        // Make sure the toast notification is displayed.
-        event.waitUntil(showNotification);
-    }
-});
+// // Respond to a server push with a user notification.
+// this.addEventListener('push', (event) => {
+//     if (Notification.permission === "granted") {
+//         const notificationText = event.data.text();
+//         const showNotification = navigator.serviceWorker.ready.then((r) => {
+//             r.showNotification('Sample PWA', {
+//                 body: notificationText
+//             });
+//         })
+//         // Make sure the toast notification is displayed.
+//         event.waitUntil(showNotification);
+//     }
+// });
 
-// Respond to the user selecting the toast notification.
-this.addEventListener('notificationclick', (event) => {
-    console.log('On notification click: ', event.notification.tag);
-    event.notification.close();
+// // Respond to the user selecting the toast notification.
+// this.addEventListener('notificationclick', (event) => {
+//     console.log('On notification click: ', event.notification.tag);
+//     event.notification.close();
 
-    // Display the current notification if it is already open, and then put focus on it.
-    event.waitUntil(clients.matchAll({
-        type: 'window'
-    }).then(function (clientList) {
-        for (var i = 0; i < clientList.length; i++) {
-            var client = clientList[i];
-            if (client.url == 'http://localhost:3000/' && 'focus' in client)
-                return client.focus();
-        }
-        if (clients.openWindow)
-            return clients.openWindow('/');
-    }));
-});
+//     // Display the current notification if it is already open, and then put focus on it.
+//     event.waitUntil(clients.matchAll({
+//         type: 'window'
+//     }).then(function (clientList) {
+//         for (var i = 0; i < clientList.length; i++) {
+//             var client = clientList[i];
+//             if (client.url == 'http://localhost:3000/' && 'focus' in client)
+//                 return client.focus();
+//         }
+//         if (clients.openWindow)
+//             return clients.openWindow('/');
+//     }));
+// });
--- PWANutriApp/src/swDev.js
+++ PWANutriApp/src/swDev.js
@@ -7,32 +7,32 @@ export default function swDev()
     });
     navigator.serviceWorker.ready.then((register)=>{
         console.warn("Service worker ready: ", register);
-        // Check if the user has an existing subscription
-        return register.pushManager.getSubscription()
-            .then(() => {
-                console.log('inside subs')
-                const vapidPublicKey = "BFb40buPpVvinr6wSgdT0Lv4yTqOsdjyK6QbRbDQrCVaT2XZOw5qM4mC1DhRoCTGgmMN8Ye3WpqU0wNTNhlwmng";
-                return register.pushManager.subscribe({
-                    userVisibleOnly: true,
-                    applicationServerKey: urlBase64ToUint8Array(vapidPublicKey)
-                });
-            });
+        // // Check if the user has an existing subscription
+        // return register.pushManager.getSubscription()
+        //     .then(() => {
+        //         console.log('inside subs')
+        //         const vapidPublicKey = "BFb40buPpVvinr6wSgdT0Lv4yTqOsdjyK6QbRbDQrCVaT2XZOw5qM4mC1DhRoCTGgmMN8Ye3WpqU0wNTNhlwmng";
+        //         return register.pushManager.subscribe({
+        //             userVisibleOnly: true,
+        //             applicationServerKey: urlBase64ToUint8Array(vapidPublicKey)
+        //         });
+        //     });
 
     })
 
-    // Utility function for browser interoperability
-    function urlBase64ToUint8Array(base64String) {
-        var padding = '='.repeat((4 - base64String.length % 4) % 4);
-        var base64 = (base64String + padding)
-            .replace(/\-/g, '+')
-            .replace(/_/g, '/');
+    // // Utility function for browser interoperability
+    // function urlBase64ToUint8Array(base64String) {
+    //     var padding = '='.repeat((4 - base64String.length % 4) % 4);
+    //     var base64 = (base64String + padding)
+    //         .replace(/\-/g, '+')
+    //         .replace(/_/g, '/');
 
-        var rawData = window.atob(base64);
-        var outputArray = new Uint8Array(rawData.length);
+    //     var rawData = window.atob(base64);
+    //     var outputArray = new Uint8Array(rawData.length);
 
-        for (var i = 0; i < rawData.length; ++i) {
-            outputArray[i] = rawData.charCodeAt(i);
-        }
-        return outputArray;
-    }
+    //     for (var i = 0; i < rawData.length; ++i) {
+    //         outputArray[i] = rawData.charCodeAt(i);
+    //     }
+    //     return outputArray;
+    // }
 }
