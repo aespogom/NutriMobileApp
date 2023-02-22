258d1f4270ead2377c4c652ef8ba57e5f647a148
Dilara <dilara.tank1@student.uva.nl>
2023-02-22 13:34:44 +0100

    cleaning unnecessary files

--- PWANutriApp/src/App.js
+++ PWANutriApp/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
-import {Navbar, Nav} from 'react-bootstrap';
+import {Navbar} from 'react-bootstrap';
 import Home from './Home'
 import { FaHome } from "react-icons/fa";
-import Model from './Model'
 
-import {Link, Route, BrowserRouter as Router, Routes} from 'react-router-dom'
+import {Route, BrowserRouter as Router, Routes} from 'react-router-dom'
 
 function App() {
   return (
@@ -15,13 +13,9 @@ function App() {
       <Navbar className='App-header'>
           <Navbar.Brand href="/"><FaHome/></Navbar.Brand>
           <Navbar.Brand>NutriApp</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/model">Model</Nav.Link>
-          </Nav>
       </Navbar>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/model" element={<Model />} />
         </Routes>
       </Router>
     </div>
